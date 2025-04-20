a6d7daa9cb8c5bc6c0ecf70b52b4061c8051ce07
Fulleni <fulleni@hotmail.com>
2025-04-20 17:54:52 +0100

    ci: set up GitHub Actions for continuous integration
    - Add workflow file for Dart package CI
    - Configure semantic pull request checks
    - Set up build and test jobs
    - Add issue templates for various types of changes
    - Update .gitignore and analysis options
    - Create initial project structure and files

---
+++ lib/src/ht_countries_inmemory.dart
@@ -0,0 +1,7 @@
+/// {@template ht_countries_inmemory}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtCountriesInmemory {
+  /// {@macro ht_countries_inmemory}
+  const HtCountriesInmemory();
+}
